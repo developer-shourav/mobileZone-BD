b0b86e95925d73641c357d66ffeefbb36499c486
Mrinmoy Raj <Mrinmoyraj823@gmail.com>
2021-11-17 10:44:23 +0600

    initial setpu complited

---
+++ src/components/Explore/Explore.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Explore = () => {
+    return (
+        <div>
+            <h1 className='text-center'> Our Products</h1>
+        </div>
+    );
+};
+
+export default Explore;
---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div className="text-center">
+            <h2>Sorr. This page is not found</h2>
+        </div>
+    );
+};
+
+export default NotFound;
